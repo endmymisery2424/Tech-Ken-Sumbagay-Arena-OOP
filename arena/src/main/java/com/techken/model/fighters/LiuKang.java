60c59085e2600b2ad5c917e6275345498be08940
Jarrell Nett D. Homecillo <jarrellnett.homecillo@cit.edu>
2026-03-09 18:58:17 +0800

    chars

--- arena/src/main/java/com/techken/model/fighters/LiuKang.java
+++ arena/src/main/java/com/techken/model/fighters/LiuKang.java
@@ -8,7 +8,7 @@ public class LiuKang extends BaseCharacter {
     public LiuKang() {
         super("Liu Kang", 130, 25, 20);
 
-        // Skills: Moderate damage AttackActions
+        // Skills: Moderate damage Attack Action
         this.skills[0] = new AttackAction("Flying Dragon Kick", 25);
         this.skills[1] = new AttackAction("Bicycle Kick", 25);
         this.skills[2] = new AttackAction("Fireball", 30);
